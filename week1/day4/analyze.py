72696627bbf8d0a1c4ffeadd6f04bff6d7d52363
Vedanth <vedanth.ramji@outlook.com>
2024-05-01 06:52:47 +0530

    ADD: variant type and molecular consequence charts to week1/day4

--- week1/day4/analyze.py
+++ week1/day4/analyze.py
@@ -2,28 +2,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('./clinvar_result.tsv', sep='\t')
-germ_line_classification = list(df['Germline classification'])
-germ_line_classification_headings = list(set(germ_line_classification))
-germ_line_classification_counts = {}
 
-fig, ax = plt.subplots()
+for a in ['Germline classification', 'Variant type', 'Molecular consequence']:
+    germ_line_classification = list(df[a])
+    germ_line_classification_headings = list(set(germ_line_classification))
+    germ_line_classification_counts = {}
 
-for i in germ_line_classification_headings:
-    if germ_line_classification.count(i) < 10:
-        continue
-    else:
-        germ_line_classification_counts.update({
-            i: (germ_line_classification.count(i)/len(germ_line_classification)*100)
-        })
+    fig, ax = plt.subplots()
 
-counts = []
-labels = []
-for i in germ_line_classification_counts.items():
-    counts.append(i[1])
-    labels.append(f'{i[0]}: {round(i[1], 2)}%')
+    for i in germ_line_classification_headings:
+        if a == 'Molecular consequence':
+            if germ_line_classification.count(i) < 100:
+                continue
+            else:
+                germ_line_classification_counts.update({
+                    i: (germ_line_classification.count(i)/len(germ_line_classification)*100)
+                })
+        else:
+            if germ_line_classification.count(i) < 10:
+                continue
+            else:
+                germ_line_classification_counts.update({
+                    i: (germ_line_classification.count(i)/len(germ_line_classification)*100)
+                })
 
-explode = [0.07 for i in range(len(germ_line_classification_counts))]
+    counts = []
+    labels = []
+    for i in germ_line_classification_counts.items():
+        counts.append(i[1])
+        labels.append(f'{i[0]}: {round(i[1], 2)}%')
 
-ax.pie(counts, labels=labels, explode=explode)
-ax.set(title='Distribution of germline classifications for genes linked to stomach cancer')
-plt.show()
+    explode = [0.1 for i in range(len(germ_line_classification_counts))]
+
+    ax.pie(counts, labels=labels, explode=explode)
+    ax.set(title=f'Distribution of {a}s for genes linked to stomach cancer')
+    plt.show()
